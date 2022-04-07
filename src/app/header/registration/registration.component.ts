b299b81c5c2f329a4421256b71a70ec3a51be07a
Quibeep <mmalecki96@gmail.com>
2022-04-07 02:36:48 +0200

    Commiting incomplete password validation - help

--- src/app/header/registration/registration.component.ts
+++ src/app/header/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorService } from './password.validator';
+
+
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registrationForm!: FormGroup
 
-  ngOnInit(): void {
+
+  constructor(private fb: FormBuilder, private v:ValidatorService) {
   }
 
+  ngOnInit(): void {
+      this.registrationForm = new FormGroup ({
+      'firstName': new FormControl(null,[Validators.required,]),
+      'lastName': new FormControl(null,[Validators.required,]),
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'city': new FormControl(null,[Validators.required,]),
+      'street': new FormControl(null,[Validators.required,]),
+      'zipCode': new FormControl(null,[Validators.required,]),
+      password: new FormControl(null,[Validators.required,]),
+      passwordConfirm: new FormControl(null,[Validators.required,]),
+    }, {validators: this.v.passwordMatch('password','passwordConfirm')}
+    );
+  }
+
+  get form() {
+    return this.registrationForm.controls;
+  }
+
+
+  registrationSubmit() {
+    console.log(this.registrationForm)
+  }
 }
