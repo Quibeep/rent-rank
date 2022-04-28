2eb9eacf5e5ca5ca140f621d0d1690d63f360fd4
Quibeep <mmalecki96@gmail.com>
2022-04-28 02:51:35 +0200

    Authentication + chips - to be finished

--- src/app/header/breadcrumbs/breadcrumbs.component.ts
+++ src/app/header/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../authentication-service';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
-  }
+    this.userSub = this.authService.user.subscribe(user =>{
+      if (!user) {
+        this.isAuthenticated = false;
+        console.log(!user)
+      } else if (user) {
+        this.isAuthenticated = true;
+        console.log(user)
+      }
+    });
+   }
 
+   ngOnDestroy(): void {
+     this.userSub.unsubscribe()
+   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { AuthService } from './authentication-service';
 import { PopupComponent } from './popup/popup.component';
 
@@ -9,17 +10,30 @@ import { PopupComponent } from './popup/popup.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
 
   constructor(private modalService: NgbModal, public authService: AuthService) { }
 
   open() {
     const modalRef = this.modalService.open(PopupComponent, {centered:true});
-
   }
 
   ngOnInit(): void {
+   this.userSub = this.authService.user.subscribe(user =>{
+     if (!user) {
+       this.isAuthenticated = false;
+       console.log(!user)
+     } else if (user) {
+       this.isAuthenticated = true;
+       console.log(user)
+     }
+   });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
   onLogout() {
