b8c2701c8300322ca1b0335491a246990af0da2a
Quibeep <mmalecki96@gmail.com>
2022-05-13 01:07:41 +0200

    Advert prototype

--- src/app/header/user.model.ts
+++ src/app/header/user.model.ts
@@ -13,12 +13,7 @@ export class User {
     return this._token;
   }
 
-  get tokenExpirationDate() {
-    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
-      return null;
-    }
-    return this._tokenExpirationDate;
-  }
+
 }
 
 
